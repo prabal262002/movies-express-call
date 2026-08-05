70cff9aa13fbfc58140b7027dd855ea21178820a
Prabal <sprabal516@gmail.com>
2026-08-06 01:28:18 +0530

    v4

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ require("dotenv").config();
 app.use(express.json());
 
 const movies = [
+
   { id: 1, title: 'Inception', director: 'Christopher Nolan', year: 2010 },
-  { id: 2, title: 'The Godfather', director: 'Francis Ford Coppola', year: 1972 }
+
+  { id: 2, title: 'The Godfather', director: 'Francis Ford Coppola', year: 1972 },
+
+  { id: 3, title: 'The Shawshank Redemption', director: 'Frank Darabont', year: 1994 }
+
 ];
 const items = [
+
   { id: 1, itemName: 'Spoon', color: 'Silver', quantity: 8},
-  { id: 2, itemName: 'Fork', color: 'Silver', quantity: 8 }
+
+ { id: 2, itemName: 'Fork', color: 'Silver', quantity: 8 },
+
+ { id: 3, itemName: 'Plate', color: 'Off-White', quantity: 6 }
+
 ];
 
 app.get('/',(req, res)=>{
@@ -35,6 +45,32 @@ app.post('/movies',(req,res)=>{
     }else{
         movies.push(newMovie);
         res.status(201).json({message:"Movie added succesfully....", movies:newMovie})
+    }
+})
+
+app.delete('/movies/:id', (req,res)=>{
+    const movieId = req.params.id;
+    const index = movies.findIndex(m=>m.id==movieId);
+
+    if(index==-1){
+        res.status(404).json({error: 'Movies not found!!'})
+    }else{
+        const movieTitle = movies[index].title;
+        movies.splice(index,1);
+        res.status(200).json({message: `Movie '${movieTitle}' deleted succesfully..`})
+    }
+})
+
+app.delete('/items/:id', (req,res)=>{
+    const itemId = req.params.id;
+    const index = items.findIndex(m=>m.id==itemId);
+
+    if(index==-1){
+        res.status(404).json({error: 'Items not found!!'})
+    }else{
+        const itemTitle = items[index].itemName;
+        items.splice(index,1);
+        res.status(200).json({message: `Item '${itemTitle}' deleted succesfully..`})
     }
 })
 
